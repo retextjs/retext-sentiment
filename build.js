3917cb1c95fd17d57ab332d314acce0f382a6140
Titus Wormer <tituswormer@gmail.com>
2017-07-31 19:12:18 -0400

    Remove superfluous comments

--- build.js
+++ build.js
@@ -1,34 +1,25 @@
 'use strict';
 
-/* eslint-env node */
-
-/* Dependencies. */
 var fs = require('fs');
-var path = require('path');
 var afinn = require('afinn-165');
 var emojiEmotion = require('emoji-emotion');
 var emoticons = require('emoticon');
 var gemoji = require('gemoji');
 
-/* Data. */
 var list = {};
 
-/* Add `afinn`. */
 Object.keys(afinn).sort().forEach(function (key) {
   list[key] = afinn[key];
 });
 
-/* Add `emoji-emotion` as unicode emoji. */
 emojiEmotion.forEach(function (info) {
   list[info.emoji] = info.polarity;
 });
 
-/* Add `emoji-emotion` as gemoji. */
 emojiEmotion.forEach(function (info) {
   list[':' + gemoji.unicode[info.emoji].name + ':'] = info.polarity;
 });
 
-/* Add `emoji-emotion` as gemoji. */
 emoticons.forEach(function (emoticon) {
   var emoji = emoticon.emoji;
   var subset = emoticon.emoticons;
@@ -42,8 +33,4 @@ emoticons.forEach(function (emoticon) {
   }
 });
 
-/* Write. */
-fs.writeFileSync(
-  path.join('index.json'),
-  JSON.stringify(list, null, 2) + '\n'
-);
+fs.writeFileSync('index.json', JSON.stringify(list, null, 2) + '\n');
